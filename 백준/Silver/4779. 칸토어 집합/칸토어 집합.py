885bb32dad0bd6cf56062b3ef4ac625f8626174f
Juno <70925118+qiakzm@users.noreply.github.com>
2024-09-30 14:56:08 +0900

    [Silver III] Title: 칸토어 집합, Time: 32 ms, Memory: 32156 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,14 @@
+# 1. Bottom-up 방식 O(N**3)
+
+ans = ['' for _ in range(13)]
+ans[0] = '-'
+
+for i in range(1, 13):
+	ans[i] = ans[i-1] + (' ' * (3 ** (i-1))) + ans[i-1]
+
+while True:
+	try:
+		N = int(input())
+		print(ans[N])
+	except:
+		break
