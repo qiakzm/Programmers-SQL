352637eb79722cbed598e23d224d59196859ff61
Juno <70925118+qiakzm@users.noreply.github.com>
2024-10-09 20:11:17 +0900

    [Silver III] Title: 숫자 야구, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+N = int(input())
+infos = [input().split() for _ in range(N)]
+
+cnt = 0
+
+for cur in permutations(range(1, 10), 3):
+	ok = True
+
+	for num, st, bl in infos:
+		cur_st = cur_bl = 0
+
+		for i in range(3):
+			if str(cur[i]) == num[i]:
+				cur_st += 1
+			elif str(cur[i]) in num:
+				cur_bl += 1
+
+		if cur_st != int(st) or cur_bl != int(bl):
+			ok = False
+			break
+
+	if ok:
+		cnt += 1
+
+print(cnt)
