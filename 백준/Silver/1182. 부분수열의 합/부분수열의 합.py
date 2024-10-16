9f914fbac3928b9b852c18ed822ca6d9ce537ca8
Juno <70925118+qiakzm@users.noreply.github.com>
2024-10-16 20:25:13 +0900

    [Silver II] Title: 부분수열의 합, Time: 260 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,16 @@
 # 부분수열을 확인하는 브루트포스
 def search(lev):
-	global N, S, arr, choose, ans
+	global N, S, arr, cur_sum, ans
 
 	if lev == N:
-		if choose and sum(choose) == S:
+		if cur_sum == S:
 			ans += 1
 		return
 
 	# 인덱스가 lev인 원소 선택 O
-	choose.append(arr[lev])
+	cur_sum += arr[lev]
 	search(lev + 1)
-	choose.pop()
+	cur_sum -= arr[lev]
 
 	# 인덱스가 lev인 원소 선택 X
 	search(lev + 1)
@@ -18,9 +18,11 @@ def search(lev):
 
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
-choose = []
+cur_sum = 0
 ans = 0
 
 search(0)
-
+if S == 0:
+	ans -= 1
+	
 print(ans)
